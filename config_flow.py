ea31e66609341f13967fd4904f55673b10b6d04b
Leo Green <leo@unixjerk.org>
2026-05-14 20:30:55 -0500

    fix config stuf

--- config_flow.py
+++ config_flow.py
@@ -1,5 +1,6 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
 
 from .const import (
     CONF_ENABLE_DEBUG_SENSOR,
@@ -12,12 +13,31 @@ from .const import (
 )
 
 
+OPTIONS_SCHEMA = vol.Schema(
+    {
+        vol.Optional(
+            CONF_ENABLE_DEBUG_SENSOR,
+            default=DEFAULT_ENABLE_DEBUG_SENSOR,
+        ): bool,
+        vol.Optional(
+            CONF_MAX_REASONABLE_HOURLY_CONS,
+            default=DEFAULT_MAX_REASONABLE_HOURLY_CONS,
+        ): vol.All(vol.Coerce(float), vol.Range(min=0)),
+        vol.Optional(
+            CONF_SKIP_NEGATIVE_CORRECTIONS,
+            default=DEFAULT_SKIP_NEGATIVE_CORRECTIONS,
+        ): bool,
+    }
+)
+
+
 class MyWaterAdvisorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     @staticmethod
+    @callback
     def async_get_options_flow(config_entry):
-        return MyWaterAdvisorOptionsFlow(config_entry)
+        return MyWaterAdvisorOptionsFlow()
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
@@ -43,24 +63,11 @@ class MyWaterAdvisorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 },
             )
 
-        schema = vol.Schema(
-            {
-                vol.Required("username"): str,
-                vol.Required("password"): str,
-                vol.Optional(
-                    CONF_ENABLE_DEBUG_SENSOR,
-                    default=DEFAULT_ENABLE_DEBUG_SENSOR,
-                ): bool,
-                vol.Optional(
-                    CONF_MAX_REASONABLE_HOURLY_CONS,
-                    default=DEFAULT_MAX_REASONABLE_HOURLY_CONS,
-                ): vol.All(vol.Coerce(float), vol.Range(min=0)),
-                vol.Optional(
-                    CONF_SKIP_NEGATIVE_CORRECTIONS,
-                    default=DEFAULT_SKIP_NEGATIVE_CORRECTIONS,
-                ): bool,
-            }
-        )
+        schema = vol.Schema({
+            vol.Required("username"): str,
+            vol.Required("password"): str,
+            **OPTIONS_SCHEMA.schema,
+        })
 
         return self.async_show_form(
             step_id="user",
@@ -68,54 +75,39 @@ class MyWaterAdvisorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
 
-class MyWaterAdvisorOptionsFlow(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
+class MyWaterAdvisorOptionsFlow(config_entries.OptionsFlowWithReload):
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        enable_debug_sensor = self.config_entry.options.get(
-            CONF_ENABLE_DEBUG_SENSOR,
-            self.config_entry.data.get(
+        suggested_values = {
+            CONF_ENABLE_DEBUG_SENSOR: self.config_entry.options.get(
                 CONF_ENABLE_DEBUG_SENSOR,
-                DEFAULT_ENABLE_DEBUG_SENSOR,
-            ),
-        )
-        max_reasonable_hourly_cons = self.config_entry.options.get(
-            CONF_MAX_REASONABLE_HOURLY_CONS,
-            self.config_entry.data.get(
-                CONF_MAX_REASONABLE_HOURLY_CONS,
-                DEFAULT_MAX_REASONABLE_HOURLY_CONS,
-            ),
-        )
-        skip_negative_corrections = self.config_entry.options.get(
-            CONF_SKIP_NEGATIVE_CORRECTIONS,
-            self.config_entry.data.get(
-                CONF_SKIP_NEGATIVE_CORRECTIONS,
-                DEFAULT_SKIP_NEGATIVE_CORRECTIONS,
-            ),
-        )
-
-        schema = vol.Schema(
-            {
-                vol.Optional(
+                self.config_entry.data.get(
                     CONF_ENABLE_DEBUG_SENSOR,
-                    default=enable_debug_sensor,
-                ): bool,
-                vol.Optional(
+                    DEFAULT_ENABLE_DEBUG_SENSOR,
+                ),
+            ),
+            CONF_MAX_REASONABLE_HOURLY_CONS: self.config_entry.options.get(
+                CONF_MAX_REASONABLE_HOURLY_CONS,
+                self.config_entry.data.get(
                     CONF_MAX_REASONABLE_HOURLY_CONS,
-                    default=max_reasonable_hourly_cons,
-                ): vol.All(vol.Coerce(float), vol.Range(min=0)),
-                vol.Optional(
+                    DEFAULT_MAX_REASONABLE_HOURLY_CONS,
+                ),
+            ),
+            CONF_SKIP_NEGATIVE_CORRECTIONS: self.config_entry.options.get(
+                CONF_SKIP_NEGATIVE_CORRECTIONS,
+                self.config_entry.data.get(
                     CONF_SKIP_NEGATIVE_CORRECTIONS,
-                    default=skip_negative_corrections,
-                ): bool,
-            }
-        )
+                    DEFAULT_SKIP_NEGATIVE_CORRECTIONS,
+                ),
+            ),
+        }
 
         return self.async_show_form(
             step_id="init",
-            data_schema=schema,
+            data_schema=self.add_suggested_values_to_schema(
+                OPTIONS_SCHEMA,
+                suggested_values,
+            ),
         )
